f234c8209bed02e879197608356e25cacf65e793
hailinds <605345320@qq.com>
2019-05-30 17:10:15 +0800

    add Spring_IOC_03_Alias

---
+++ Spring_IOC_03_Alias/src/com/dreamguard/test/Test01.java
@@ -0,0 +1,16 @@
+package com.dreamguard.test;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.dreamguard.domain.Person;
+
+public class Test01 {
+	@Test
+	public void test01() throws Exception {
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Person person = (Person)context.getBean("p");
+		System.out.println(person);
+	}
+}
